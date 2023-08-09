a126d293fc97604362b6df49c75d4fe0e1065e5c
thiagoephigenio <thiago.costa@medcloud.co>
2023-08-09 00:09:31 -0300

    chore: add jest config

--- jest.config.ts
+++ jest.config.ts
@@ -1,12 +1,12 @@
-import type { Config } from "jest";
+import type { Config } from 'jest';
 
 const config: Config = {
   collectCoverage: true,
-  collectCoverageFrom: ["<rootDir>/src/**/*.ts"],
-  coverageDirectory: "coverage",
-  coverageProvider: "v8",
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
   transform: {
-    ".+\\.ts$": "ts-jest",
+    '.+\\.ts$': 'ts-jest',
   },
 };
 
