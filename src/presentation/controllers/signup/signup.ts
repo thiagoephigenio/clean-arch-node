8e480294f4ba19515b2491413acd34e5f39a68ea
thiagoephigenio <thiago.costa@medcloud.co>
2023-08-15 22:01:47 -0300

    feat: ensure SignUpController returns 200 if valid data is provided

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -40,15 +40,14 @@ export class SignUpController implements Controller {
       if (!isValid) {
         return badRequest(new InvalidParamError('email'));
       }
-      this.addAccount.add({
+      const account = this.addAccount.add({
         name,
         email,
         password,
       });
-
       return {
         statusCode: 200,
-        body: 'ok',
+        body: account,
       };
     } catch (error) {
       return serverError();
