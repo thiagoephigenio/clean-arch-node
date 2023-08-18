9f921dbd772c75e5612ee678bab493b4057cdbb5
thiagoephigenio <thiago.costa@medcloud.co>
2023-08-17 22:31:41 -0300

    refactor: move sut creation to a factory method

--- src/data/useCases/add-account/db-add-account.spec.ts
+++ src/data/useCases/add-account/db-add-account.spec.ts
@@ -1,16 +1,31 @@
+import { Encrypter } from 'src/data/protocols/encrypter';
 import { DbAddAccount } from './db-add-account';
+
+interface SutTypes {
+  sut: DbAddAccount;
+  encrypterStub: Encrypter;
+}
+
+const makeSut = (): SutTypes => {
+  class EncrypterStub {
+    async encrypt(value: string): Promise<string> {
+      return new Promise((resolve) => resolve('hased_password'));
+    }
+  }
+
+  const encrypterStub = new EncrypterStub();
+  const sut = new DbAddAccount(encrypterStub);
+
+  return {
+    sut,
+    encrypterStub,
+  };
+};
 
 describe('DbAccount UseCase', () => {
   test('Should call Encripter with correct password', () => {
-    class EncrypterStub {
-      async encrypt(value: string): Promise<string> {
-        return new Promise((resolve) => resolve('hased_password'));
-      }
-    }
-
-    const encryptStub = new EncrypterStub();
-    const sut = new DbAddAccount(encryptStub);
-    const encryptSpy = jest.spyOn(encryptStub, 'encrypt');
+    const { sut, encrypterStub } = makeSut();
+    const encryptSpy = jest.spyOn(encrypterStub, 'encrypt');
     const accountData = {
       name: 'valid_name',
       email: 'valid_email',
