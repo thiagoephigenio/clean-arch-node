0e1631556b376a2d49a3700748674031ba4e1460
thiagoephigenio <thiago.costa@medcloud.co>
2023-08-24 21:54:48 -0300

    feat: ensure BcryptAdapter calls bcrypt with correct values

---
+++ src/infra/criptography/bcrypt-adapter.ts
@@ -0,0 +1,16 @@
+import { Encrypter } from 'src/data/protocols/encrypter';
+import * as bcrypt from 'bcrypt';
+
+export class BcrypterAdapter implements Encrypter {
+  private readonly salt: number;
+
+  constructor(salt: number) {
+    this.salt = salt;
+  }
+
+  async encrypt(value: string): Promise<string> {
+    await bcrypt.hash(value, this.salt);
+
+    return '';
+  }
+}
